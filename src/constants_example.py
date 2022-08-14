a96bb123e9b491b6285a34bc85ee926160300381
bfs07 <bfs.07@hotmail.com>
2022-08-14 19:00:34 -0300

    Update Sendgrid API_KEY

--- src/constants_example.py
+++ src/constants_example.py
@@ -12,12 +12,12 @@ CATEGORY = ''
 # Ex: 1007, 1406, 1503
 PAYMENT_CODE = 0000
 
-# From SendGrid email API
+# From SendGrid email API. Ex: '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'
 SENDGRID_API_KEY = ''
 
 SENDER_EMAIL = ''
 
 RECIPIENT_EMAIL = ''
 
-# Website address. Ex: the-name-of-your-app.herokuapp.com
+# Website address. Ex: 'the-name-of-your-app.herokuapp.com'
 HEROKU_ADDRESS = ''
